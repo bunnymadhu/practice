d0354d1be36c25edc76470056f4b49e091d36a67
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-06-04 19:17:03 +0530

    make changes in json.sh file

--- jason.sh
+++ jason.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
 
+## Markup language : (XML,JSON,YAML)
+## key = values
+## key = multiple values (list)
+## key =  key ; value (map/dictionary/Hash)
+
+## Json :
+
+ {
+    "fruit" : "apple",
+    "count" : 100,
+     "types" : [ "red", "green" ],
+     "types_count" : {
+       "red" : 100,
+       "green" : 200
+     }
+ }
+
+ ## in jason for example we can edit some file
+ ## vim /tmp/l.json (in case any edit changes we can press 'q') then after it will wrk.
+ ## cat /tmp/l/json | jq .fruit   etcc...we can search like that
