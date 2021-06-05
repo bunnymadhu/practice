52ff6a5c3e93f6ef410a1aef1e5d912d0df9d781
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-06-05 16:13:14 +0530

    make changes in madhu.sh file

--- madhu.sh
+++ madhu.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo  "want to know your name: "
-$1
+## what is DNS server:
+## The Domain Name System (DNS) is the phonebook of the Internet. When users type domain names such as ‘google.com’ or ‘nytimes.com’ into web browsers, DNS is responsible for finding the correct IP address for those sites. Browsers then use those addresses to communicate with origin servers or CDN edge servers to access website information. This all happens thanks to DNS servers: machines dedicated to answering DNS queries.
