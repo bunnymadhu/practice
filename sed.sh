0fb78c4ffff5019ed63847d663ee869cf5785f59
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-06-05 10:27:03 +0530

    make changes in frontend.sh file

--- sed.sh
+++ sed.sh
@@ -8,16 +8,16 @@
 ## sed 's/root/ROOT/' passwd                                                                ## c : change and  replace that line
                                                                                                              ## a : add a word
 ## when u substitute or replace  wherever  like 'root' in that file,                 ## i : insert a word
-## ## sed 's/root/ROOT/g' passwd
+## sed 's/root/ROOT/g' passwd
 
 ## when u  multiple times words will change   'root' and 'network' in that file,
-## ## sed -e 's/root/ROOT/g'  -e 's/network/NETWORK/g' passwd
+## sed -e 's/root/ROOT/g'  -e 's/network/NETWORK/g' passwd
 
 ## when u  give case sensitive and capital words u shold give   'root' and 'network' in that file,
-## ## sed -e 's/root/ROOT/ig'  -e 's/network/NETWORK/ig' passwd
+## sed -e 's/root/ROOT/ig'  -e 's/network/NETWORK/ig' passwd
 
 ## when u  change the words in that file  like   'root' and 'network' in that file,
-## ## sed -i -e 's/root/ROOT/g'  -e 's/network/NETWORK/g' passwd
+## sed -i -e 's/root/ROOT/g'  -e 's/network/NETWORK/g' passwd
 ## -i means -- cat passwd (it will change in this file )
 
 ## when u add a word,
