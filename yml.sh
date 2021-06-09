79ccdaf5e9dcaca5b60bbf76baf0ff27912441aa
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-06-09 21:55:15 +0530

    make changes in yml.sh file

---
+++ yml.sh
@@ -0,0 +1,11 @@
+coursename: "DevOps"
+trainername: "Raghu"
+timings:
+  - 600IST
+  - 730IST
+topics:
+ aws:
+  - EC2
+  - S3
+ devops: [ "Ansible" "Linux" "Docker" ]
+phoneNumbers: { personal: 999,mobile: 888 }
