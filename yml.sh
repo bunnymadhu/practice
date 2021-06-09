80b7f50e1eb4cd5ba9093171abea8f16633df7fc
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-06-09 22:04:05 +0530

    make changes in yml.sh file

--- yml.sh
+++ yml.sh
@@ -9,3 +9,15 @@ topics:
   - S3
  devops: [ "Ansible" "Linux" "Docker" ]
 phoneNumbers: { personal: 999,mobile: 888 }
+
+
+
+
+
+VARS:
+   PACKAGE_NAME:httpd
+tasks:
+- name:install the latest verion of Apache
+   yum:
+    name:${PACKAGE_NAME}
+    state:latest
